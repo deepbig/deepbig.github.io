30bd20a7bc4b6a7f67e908c4ae5343b22d6c3984
Hongsuk Ryu <hryu@grad.bryant.edu>
2019-11-18 15:39:34 -0800

    TDD_first_app first commit

---
+++ recent02/precache-manifest.6835e2940e4111f60aa8a76032017063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f71ff4dcf0d26b62ee22d203ef3345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ce5a5eb618248ae3a7",
+    "url": "/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "b4e915f71b317c3306d7",
+    "url": "/static/js/2.00c8450a.chunk.js"
+  },
+  {
+    "revision": "67ce5a5eb618248ae3a7",
+    "url": "/static/js/main.39a6fdfc.chunk.js"
+  },
+  {
+    "revision": "970dcc8236bc5782c111",
+    "url": "/static/js/runtime-main.09c573df.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
